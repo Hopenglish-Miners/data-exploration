c7aa244c545ae13d4d5a33d3e16f7db5621802ae
nweat <nikkiweat@gmail.com>
2016-12-02 00:31:23 +0800

    listen score stats'

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import numpy as np
 import pandas as pd
 
 def videoCSVToJSON(ifile):
-	"""
-	convert videoDataInfo.csv to json
-	"""
 	videos = []
 	inputFile = codecs.open(ifile, 'r', encoding = "utf-8")
 
@@ -24,11 +21,7 @@ def videoCSVToJSON(ifile):
 	inputFile.close()
 	return videos[1:]
 
-def listen_scores_overall_stats(ifile1, ifile2):
-	"""
-	generate overall stats for listen scores
-	"""
-	listenScores = []
+def loadJSONFiles(ifile1, ifile2):
 	loadfile = []
 	loadfile.append(ifile1)
 	loadfile.append(ifile2)
@@ -36,15 +29,48 @@ def listen_scores_overall_stats(ifile1, ifile2):
 	for f in loadfile:
 		with open(f) as data_file:
 			all_data = json.load(data_file)
+	return all_data
+
+def getListenScores(all_data):
+	listenScores = []
 	
 	for b in all_data:
 		for s in b['listenScore']:
 			listenScores.append(s)
-	
-	scores = pd.DataFrame.from_records(listenScores)
-	grouped = scores.groupby('postId')
+	return listenScores
+
+def listenScoresOverallStats(ifile1, ifile2):
+	"""
+	generate overall stats for listen scores
+	"""
+	all_data = loadJSONFiles(ifile1, ifile2)
+	listenScores = getListenScores(all_data)
+	all_scores = pd.DataFrame.from_records(listenScores)
+	grouped = all_scores.groupby('postId')
 	return grouped.describe()
 
+def listen_score_range(dl):
+	if dl < -1: return '< -1'
+	elif dl == -1: return 'Incomplete'
+	elif 0 <= dl <= 30: return '0-30'
+	elif 30 < dl <= 60: return '30-60'
+	elif 60 < dl <= 80: return '60-80'
+	elif 80 < dl <= 100: return '80-100'
+	else: return 'None'
+
+def listenScoreStats(ifile1, ifile2):
+	"""
+	DESCRIPTION: For each video, group listen scores based on range, could be more useful to group by section as well
+	REFERENCES:
+	http://stackoverflow.com/questions/36723625/pandas-sql-case-statement-equivalent
+	http://stackoverflow.com/questions/16349389/grouping-data-by-value-ranges
+	"""
+	all_data = loadJSONFiles(ifile1, ifile2)
+	listenScores = getListenScores(all_data)
+	df = pd.DataFrame.from_records(listenScores)
+	df['score_range'] = df['score'].map(listen_score_range)
+	results = df.groupby(["postId","score_range"]).size()
+	return results.unstack()
    
 def main(argv):
 	"""
@@ -63,7 +89,8 @@ def main(argv):
 				ifile2 = arg
 
 		pd.set_option("display.max_rows", 5000)
-		print listen_scores_overall_stats(ifile, ifile2)
+		pd.set_option("display.max_columns", 500)
+		print listenScoreStats(ifile, ifile2)
 
 	except arg:
 		print 'Arguments parser error ' + arg
